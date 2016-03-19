90666b49acf078c9a68c89ee1a3a14cf074efc1d
Jacob Martin <mindnumb@gmail.com>
2016-03-18 17:11:07 -0700

    Foreach loops

--- src/com/junkgrave/space/background/Stars.java
+++ src/com/junkgrave/space/background/Stars.java
@@ -3,12 +3,10 @@ package com.junkgrave.space.background;
 import java.awt.*;
 
 public class Stars {
-	private int numberOfStars;
 
 	private Star[] stars;
 
 	public Stars(int numberOfStars) {
-		this.numberOfStars = numberOfStars;
 		stars = new Star[numberOfStars];
 		for (int i = 0; i < stars.length; ++i) {
 			stars[i] = new Star();
@@ -16,14 +14,14 @@ public class Stars {
 	}
 
 	public void tick() {
-		for (int i = 0; i < stars.length; ++i) {
-			stars[i].tick();
+		for (Star star : stars) {
+			star.tick();
 		}
 	}
 
 	public void render(Graphics g) {
-		for (int i = 0; i < stars.length; ++i) {
-			stars[i].render(g);
+		for (Star star : stars) {
+			star.render(g);
 		}
 	}
 }
